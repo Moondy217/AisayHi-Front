82521eeb6752129e52ff1fb9cee55b8fd5cc5123
문다예 <pt07147@gmail.com>
2024-08-13 09:57:42 +0900

    0813 다예 footer 수정

--- tpm/src/pages/main/footer.js
+++ tpm/src/pages/main/footer.js
@@ -16,7 +16,7 @@ function Footer() {
             개인정보처리방침 <span className={styles.footerTextGrey}> | </span> 이용약관
           </span>
           <span>(주)더포터마켓 대표 : 손예지 <span className={styles.footerTextGrey}> | </span> 
-          TEL:02-0929-1234<span className={styles.footerTextGrey}> | </span>theportermarket@kw.ac.kr</span>
+          TEL: 02-0929-1234<span className={styles.footerTextGrey}> | </span>theportermarket@kw.ac.kr</span>
 
           <span>서울특별시 노원구 광운로 20<span className={styles.footerTextGrey}> | </span>
           통신판매업 신고 : 2024-서울노원-0001호<span className={styles.footerTextGrey}> | </span>
